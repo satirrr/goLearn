cd3dc44302665093ab46ec0c656527095ea877fc
Musa <musamhtr23@gmail.com>
2023-02-15 21:58:20 +0700

    init

---
+++ graph/resolver.go
@@ -0,0 +1,7 @@
+package graph
+
+//go:generate go get github.com/99designs/gqlgen@v0.17.24
+//go:generate go get github.com/99designs/gqlgen/codegen@v0.17.24
+//go:generate go run github.com/99designs/gqlgen
+
+type Resolver struct{}
